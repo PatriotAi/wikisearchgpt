190fe6519978d601d26f33bd0f427ff7f7694b83
PatriotAi <jurchik87@googlemail.com>
2025-09-08 22:50:12 +0200

    Update Loader.jsx

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div className="mt-6">
-      <div className="w-48 h-2 bg-gray-200 rounded-full overflow-hidden">
-        <div className="h-full bg-blue-600 animate-pulse"></div>
+    <div className="loader-box">
+      <div className="progress-bar">
+        <div className="progress"></div>
       </div>
-      <p className="mt-2 text-gray-500">Набираємо ваш запит...</p>
+      <p className="loading-text">Набираємо ваш запит...</p>
     </div>
   );
 }
